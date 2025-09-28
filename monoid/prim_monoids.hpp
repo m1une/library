a03c138ab15dfa92733a10d1d6c659a8f695150d
みうね <54197251+m1une@users.noreply.github.com>
2025-09-29 01:06:44 +0900

    fix path

--- monoid/prim_monoids.hpp
+++ monoid/prim_monoids.hpp
@@ -1,14 +1,15 @@
 #ifndef M1UNE_PRIM_MONOIDS_HPP
 #define M1UNE_PRIM_MONOIDS_HPP 1
 
-#include "../monoid.hpp"
+#include "monoid.hpp"
 #include "monoids/add_monoid.hpp"
-#include "mul_monoid.hpp"
-#include "min_monoid.hpp"
-#include "max_monoid.hpp"
-#include "and_monoid.hpp"
-#include "or_monoid.hpp"
-#include "xor_monoid.hpp"
-#include "affine_monoid.hpp"
+#include "monoids/mul_monoid.hpp"
+#include "monoids/min_monoid.hpp"
+#include "monoids/max_monoid.hpp"
+#include "monoids/and_monoid.hpp"
+#include "monoids/or_monoid.hpp"
+#include "monoids/xor_monoid.hpp"
+#include "monoids/affine_monoid.hpp"
+#include "monoids/affine_right_monoid.hpp"
 
 #endif  // M1UNE_PRIM_MONOIDS_HPP
