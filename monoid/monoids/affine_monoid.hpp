fd2e63bdd7ce278887081538f75e269cbf07d4c1
みうね <54197251+m1une@users.noreply.github.com>
2025-09-28 20:34:43 +0900

    add comments

--- monoid/monoids/affine_monoid.hpp
+++ monoid/monoids/affine_monoid.hpp
@@ -7,6 +7,9 @@
 
 namespace m1une {
 template <typename T>
+// Affine transformation f(x) = ax + b is represented as (a, b)
+// perform f first, then g
+// op(f, g)(x) = g(f(x))
 using affine_monoid = monoid<std::pair<T, T>,
                              [](std::pair<T, T> f, std::pair<T, T> g) {
                                  return std::pair<T, T>(f.first * g.first, f.second * g.first + g.second);
