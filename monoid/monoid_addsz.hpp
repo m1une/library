b7397530ebabbb7d7b7d492cdb3ce88b9e150ffd
みうね <54197251+m1une@users.noreply.github.com>
2025-09-29 01:30:55 +0900

    add monoid_addsz

---
+++ monoid/monoid_addsz.hpp
@@ -0,0 +1,25 @@
+#ifndef M1UNE_MONOID_ADDSZ_HPP
+#define M1UNE_MONOID_ADDSZ_HPP 1
+
+#include <concepts>
+
+#include "monoid.hpp"
+
+namespace m1une {
+
+template <typename T>
+struct value_and_size {
+    T value;
+    int size;
+};
+
+template <monoid_concept M>
+using monoid_addsz = monoid<value_and_size<typename M::value_type>,
+                            [](value_and_size<typename M::value_type> a, value_and_size<typename M::value_type> b) {
+                                return value_and_size<typename M::value_type>{M::op(a.value, b.value), a.size + b.size};
+                            },
+                            []() { return value_and_size<typename M::value_type>{M::id(), 0}; }, M::is_commutative>;
+
+}  // namespace m1une
+
+#endif  // M1UNE_MONOID_ADDSZ_HPP
