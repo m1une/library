48978d5f64767b4b11146877ddff0c1b1ff78eae
みうね <54197251+m1une@users.noreply.github.com>
2026-06-04 16:47:39 +0900

    feat: acted monoid concept and wrapper

---
+++ acted_monoid/concept.hpp
@@ -0,0 +1,32 @@
+#ifndef M1UNE_ACTED_MONOID_CONCEPT_HPP
+#define M1UNE_ACTED_MONOID_CONCEPT_HPP 1
+
+#include <concepts>
+
+namespace m1une {
+namespace acted_monoid {
+
+// Concept defining the requirements for an Acted Monoid.
+template <typename AM>
+concept IsActedMonoid = requires(
+    typename AM::value_type a, typename AM::value_type b,
+    typename AM::operator_type f, typename AM::operator_type g
+) {
+    // 1. Value Monoid
+    typename AM::value_type;
+    { AM::id() } -> std::same_as<typename AM::value_type>;
+    { AM::op(a, b) } -> std::same_as<typename AM::value_type>;
+
+    // 2. Operator Monoid
+    typename AM::operator_type;
+    { AM::op_id() } -> std::same_as<typename AM::operator_type>;
+    { AM::op_comp(f, g) } -> std::same_as<typename AM::operator_type>; // Composition order: f(g(x))
+
+    // 3. Mapping: Operator x Value -> Value
+    { AM::mapping(f, a) } -> std::same_as<typename AM::value_type>;
+};
+
+}  // namespace acted_monoid
+}  // namespace m1une
+
+#endif  // M1UNE_ACTED_MONOID_CONCEPT_HPP
---
+++ acted_monoid/wrapper.hpp
@@ -0,0 +1,51 @@
+#ifndef M1UNE_ACTED_MONOID_WRAPPER_HPP
+#define M1UNE_ACTED_MONOID_WRAPPER_HPP 1
+
+namespace m1une {
+namespace acted_monoid {
+
+// Wrapper struct to generate an Acted Monoid using Non-Type Template Parameters (NTTP).
+// Useful for quickly defining acted monoids using custom functions or constexpr lambdas during contests.
+template <
+    typename T, 
+    typename E, 
+    auto Op, 
+    auto Id, 
+    auto OpComp, 
+    auto OpId, 
+    auto Mapping
+>
+struct Wrapper {
+    using value_type = T;
+    using operator_type = E;
+
+    // Returns the identity element of the value monoid.
+    static constexpr T id() {
+        return Id();
+    }
+
+    // Returns the result of the value monoid binary operation.
+    static constexpr T op(const T& a, const T& b) {
+        return Op(a, b);
+    }
+
+    // Returns the identity element of the operator monoid.
+    static constexpr E op_id() {
+        return OpId();
+    }
+
+    // Composes two operations f and g (corresponds to f(g(x))).
+    static constexpr E op_comp(const E& f, const E& g) {
+        return OpComp(f, g);
+    }
+
+    // Applies the operator f onto the value x.
+    static constexpr T mapping(const E& f, const T& x) {
+        return Mapping(f, x);
+    }
+};
+
+}  // namespace acted_monoid
+}  // namespace m1une
+
+#endif  // M1UNE_ACTED_MONOID_WRAPPER_HPP
