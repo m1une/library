74985b687ef114a2de6a169c61ea816f18849f98
みうね <54197251+m1une@users.noreply.github.com>
2026-06-14 03:24:49 +0900

    add dynamic array test

---
+++ verify/data_structure/dynamic_array.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ITP2_1_C"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "data_structure/dynamic_array.hpp"
+
+int main() {
+    int cursor = 0;
+    m1une::data_structure::DynamicArray<int> L;
+    int q;
+    cin >> q;
+    while (q--) {
+        int t;
+        cin >> t;
+        if (t == 0) {
+            int x;
+            cin >> x;
+            L.insert(cursor, x);
+        } else if (t == 1) {
+            int d;
+            cin >> d;
+            cursor += d;
+        } else {
+            L.erase(cursor);
+        }
+    }
+    vector<int> ans = L.to_vector();
+    for (int x : ans) {
+        cout << x << '\n';
+    }
+}
