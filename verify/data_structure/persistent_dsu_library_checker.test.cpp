10be77ae2470eaac211a037ede58c3642c327b5f
みうね <54197251+m1une@users.noreply.github.com>
2026-06-15 03:38:29 +0900

    add persistent dsu

---
+++ verify/data_structure/persistent_dsu_library_checker.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/persistent_unionfind"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "data_structure/persistent_dsu.hpp"
+using namespace m1une::data_structure;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, Q;
+    cin >> N >> Q;
+    vector<PersistentDsu> dsus;
+    dsus.push_back(PersistentDsu(N));
+    while (Q--) {
+        int t, k, u, v;
+        cin >> t >> k >> u >> v;
+        if (t == 0) {
+            dsus.push_back(dsus[k + 1].merge(u, v));
+        } else {
+            cout << int(dsus[k + 1].same(u, v)) << '\n';
+            dsus.push_back(PersistentDsu(0));
+        }
+    }
+}
