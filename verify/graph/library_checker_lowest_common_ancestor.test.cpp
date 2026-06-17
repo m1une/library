d89dbb7d70db8ea07749d89b0329e8a87bf4231f
みうね <54197251+m1une@users.noreply.github.com>
2026-06-18 02:21:34 +0900

    add library checker lowest common ancestor test

---
+++ verify/graph/library_checker_lowest_common_ancestor.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/lca"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "graph/graph.hpp"
+#include "tree/sparse_table_lca.hpp"
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, Q;
+    cin >> N >> Q;
+    m1une::graph::Graph<> g(N);
+    for (int i = 1; i < N; ++i) {
+        int p;
+        cin >> p;
+        g.add_edge(p, i);
+    }
+    m1une::tree::SparseTableLca<> lca(g, 0);
+    while (Q--) {
+        int u, v;
+        cin >> u >> v;
+        cout << lca.lca(u, v) << '\n';
+    }
+}
